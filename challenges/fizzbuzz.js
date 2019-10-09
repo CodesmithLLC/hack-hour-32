03916afe324743afb2289d4c75520761bbf28a8e
evanking11 <evanking11@gmail.com>
2019-10-09 09:35:53 -0700

    fizzbuzz challenge completed

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,4 +1,7 @@
-// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
+// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of
+// numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of n
+// umbers divisble by both 3 and 5
+
 // fizzbuzz(16);  -> [ 1,
 //                     2,
 //                     'fizz',
@@ -17,7 +20,20 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  const fb = [];
+  // ADD CODE HERE
+  for (let i = 0; i < num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      fb.push('fizzbuzz');
+    } else if (i % 5 === 0) {
+      fb.push('buzz');
+    } else if (i % 3 === 0) {
+      fb.push('fizz');
+    } else {
+      fb.push(i);
+    }
+  }
+  return fb;
 }
 
 module.exports = fizzbuzz;
