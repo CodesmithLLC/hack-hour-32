cbc8fa37f80a717acc54880fbbc0b9be3d3a9b0b
aburkett1 <austin_osten@hotmail.com>
2019-10-09 09:28:04 -0700

    Completed Hack Hour - Fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,4 +1,5 @@
-// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
+// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, 
+// "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
 // fizzbuzz(16);  -> [ 1,
 //                     2,
 //                     'fizz',
@@ -17,7 +18,19 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+    const fizzbuzzArr = [];
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            fizzbuzzArr.push('fizzbuzz');
+        } else if (i % 3 === 0) {
+            fizzbuzzArr.push('fizz');
+        } else if (i % 5 === 0) {
+            fizzbuzzArr.push('buzz');
+        } else {
+            fizzbuzzArr.push(i);
+        }
+    }
+    return fizzbuzzArr;
 }
 
 module.exports = fizzbuzz;
