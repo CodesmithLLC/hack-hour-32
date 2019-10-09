a54e502fe5949ac8ac8d2c7c3ff52694ba9f1681
Jourdan Ball <jourdan.ball@gmail.com>
2019-10-09 09:22:21 -0700

    Update fizzbuzz.js

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -10,14 +10,26 @@
 //                     'fizz',
 //                     'buzz',
 //                     11,
-//                     'fizz',
+//                     'fizz', 
 //                     13,
 //                     14,
 //                     'fizzbuzz',
 //                     16 ]
 
 function fizzbuzz(num) {
-
+    let array = []
+    for(let i = 1; i <= num; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            array.push("fizzbuzz");
+        } else if(i % 3 === 0){
+            array.push("fizz");
+        } else if(i % 5 === 0){
+            array.push("buzz");
+        } else {
+            array.push(i);
+        }
+    } 
+    return array
 }
 
 module.exports = fizzbuzz;
