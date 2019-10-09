1d9597730082539bcbc345fcbf2c6bbabcbb584a
Mychal Estalilla <mychal.es@protonmail.com>
2019-10-09 09:20:30 -0700

    complete fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,25 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+  const result = [];
+
+  for (let i = 1; i <= num; i++) {
+	if (i % 15 === 0) {
+	  result.push('fizzbuzz');
+	} else if (i % 3 === 0) {
+	  result.push('fizz')
+	} else if (i % 5 === 0) {
+	  result.push('buzz');
+	} else {
+	  result.push(i);
+	}
+  }
+
+  return result;
+
 
 }
+
+//console.log(fizzbuzz(16));
 
 module.exports = fizzbuzz;
