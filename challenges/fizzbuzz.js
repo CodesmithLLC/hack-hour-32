0a3e255556e79b1f6c15c53f4227dabda3ffddec
Connie <clai011@ucr.edu>
2019-10-09 09:17:26 -0700

    done with first HH

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,28 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+    // create array
+    const arr = [];
+    // create variable and initialize to 1
+    let i = 1;
+    // loop from 1 to num, push num i into array
+    while (i <= num) {
+        // if num is divisible by both 3 and 5, push 'fizzbuzz'
+        if (i % 15 === 0) {
+            arr.push('fizzbuzz');
+        } else if (i % 3 === 0) { // if num is divisible by 3, push 'fizz'
+            arr.push('fizz');
+        } else if (i % 5 === 0) { //  'buzz' for nums divisible by 5
+            arr.push('buzz');
+        } else {        // else not divisible by 3, 5, 15
+            arr.push(i);
+        }
+        // increment i
+        i++;
+    }
+    // return array from 1 to num
+    console.log(arr.length)
+    return arr;
 }
 
 module.exports = fizzbuzz;
