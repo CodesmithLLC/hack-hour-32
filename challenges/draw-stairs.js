761ac8e7c9892943d439521655bad9a4b2e44e3e
Mychal Estalilla <mychal.es@protonmail.com>
2019-10-11 09:43:56 -0700

    complete draw-stairs.js

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -13,9 +13,19 @@
 
 */
 
-function drawStairs(n) {
-
+function drawStairs(n, i = 0) {
+  const step = '*';
+  const space = ' ';
+ 
+  while (n+1 > 0) {
+	// draw code
+	if (i !== 0) {
+	  console.log(space.repeat(n)+step.repeat(i));
+	}
+	return drawStairs(n-1, i+1);
+  }
 }
 
+drawStairs(6);
 
 module.exports = drawStairs;
