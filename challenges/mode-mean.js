d459434ed472eb7a739f654f7ad567141e1ade92
Mychal Estalilla <mychal.es@protonmail.com>
2019-10-12 09:39:52 -0700

    complete mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,35 @@
 
 
 function modemean(array) {
+  const counter = {};
+  let total = 0;
+  let max = 0;
+
+  // loop through array 
+  array.forEach( item => {
+	/*** MODE ***/
+	// initialize key-vals
+	if (counter[item] === undefined) counter[item] = 0;
+	// increment counter key-vals
+	counter[item] = counter[item] += 1;
+	// set max
+	if (counter[item] > max) max = counter[item];
+
+	/*** MEAN ***/
+	//increase counter
+	total += item;
+  });
+
+  const average = total/array.length;
+
+  if (Math.floor(average) === max) return true;
+  return false;
 
 }
+
+
+//console.log('expect true. Got => ' + modemean([1,2,2,3]));
+//console.log('expect false. Got => ' + modemean([1,1,2,3]));
+
 
 module.exports = modemean;
