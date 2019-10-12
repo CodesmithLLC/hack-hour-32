e88c825cd9343de81559ac32312a5b907dc9b7f4
Scott McInnis <scottalyst@gmail.com>
2019-10-12 09:19:56 -0700

    Committing again

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -13,20 +13,32 @@
 
 */
 
-function drawStairs(n) {
-  for (let i = 1; i <= n; i++) {
-    let stairString = '';
-    let spaces = n - i;
+// function drawStairs(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let stairString = '';
+//     let spaces = n - i;
 
-    for (let j = 0; j < spaces; j++) {
-      stairString += ' ';
-    }
-    for (let k = 0; k < i; k++) {
-      stairString += '*'
-    }
-    console.log(stairString);
-  }
+//     for (let j = 0; j < spaces; j++) {
+//       stairString += ' ';
+//     }
+//     for (let k = 0; k < i; k++) {
+//       stairString += '*'
+//     }
+//     console.log(stairString);
+//   }
+// }
+
+function drawStairs(n) {
+  let stairs = '';
+  if (n = 0) { return '*' }
+  else { return drawStairs(n - 1) };
+
+  console.log(stairs);
 }
 
+function drawStairs(5);
 
 module.exports = drawStairs;
+
+
+
