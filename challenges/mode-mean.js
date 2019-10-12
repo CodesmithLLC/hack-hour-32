20bb84255d55247b193ef1702e4858bdf22cda5f
Scott McInnis <scottalyst@gmail.com>
2019-10-12 09:43:04 -0700

    Hack Hour - mode-mean Attempt 1. I believe this works but need to find a test case to return true

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,32 @@
 
 
 function modemean(array) {
+	// find mean
+	let modeStore = {}
+	let sum = array.reduce((acc, element) => {
+		// store all values
+		if (modeStore[element] === undefined) {
+			modeStore[element] += 1;
+		}
+		return acc + element
+	}, 0);
+	let median = Math.floor(sum / array.length);
+	// find mode
+	let modeEntries = Object.entries(modeStore);
 
+	let modePair = modeEntries.reduce( (acc, modeEntry) => {
+		if (modeEntry[1] >= acc[1]) {
+			return modeEntry;
+		} else {
+			return acc;
+		}
+	});
+
+	let mode = modePair[0];
+
+	return (mode === mean);
+
+	//, [modeEntries[0][0], modeEntries[0][1]]
 }
 
 module.exports = modemean;
