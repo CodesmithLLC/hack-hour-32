167fd105e2b60e315375f32b40c771c3029b60ba
Justin <humbleyetconfidentdeveloper@gmail.com>
2019-10-12 09:39:20 -0700

    mode-mean finished

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,31 @@
 
 
 function modemean(array) {
+    let mean = 0;
+    for (let i = 0; i < array.length; i++) {
+        mean += array[i];
+    }
+    mean = mean / array.length;
 
+    const obj = {};
+    let counter = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (!obj[array[i]]) obj[array[i]] = counter; 
+        obj[array[i]]++;
+    }
+    let mode = 0;
+    for (let key in obj) {
+        if (obj[key] > mode) mode = obj[key]; 
+    }
+    if (mean === mode) return true;
+    else return false;
 }
+
+var arr = [2,2,2,1,5,8,1];
+console.log(modemean(arr)); // expected 'true'
+var arr = [2,2,2,1,5,8,2];
+console.log(modemean(arr)); // expected 'false'
+
+
 
 module.exports = modemean;
