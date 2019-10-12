464c78c2358d1d2e18e88abcd0aa56367f469a21
Eric Wise <angiechangpagne@gmail.com>
2019-10-12 09:35:39 -0700

    mode mean 2

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -12,6 +12,7 @@
 
 function modemean(array) {
     let result=false;
+    console.log(array);
     //mode is frequency of occurences
     if(array===undefined || array.length===0){
         result=false;
@@ -21,7 +22,7 @@ function modemean(array) {
         result=true;
         return result;
     }
-    let mode=1;
+    let mode=1; //elements each exist
     let modeElement=array[0];
     let sum;
     let mean;
@@ -33,25 +34,35 @@ function modemean(array) {
         let count=1;
         //counts the frequency of el
         for(let j=i;j<array.length;j++){
+            //if index j is at .length-mode-count we can break out sooner
             //we do not need to recount begining so we start at i to shorten the circuit
-            if(array[j]===el){count++};
+            if(array[j]===el){count++;}
+            console.log("in the count");
             //another idea is to use the strin and remove chars and compare the lenghts
+        }
+        if(count===mode){
+            //element with same frequency, use max
+            if(modeElement<el){
+                modeElement=el;
+                mode=count;
+            }
         }
         if(count>mode){
             modeElement=el;
             mode=count;
         }
-
+        
     }
-    mean=sum/array.length;
+    mean=Math.floor(sum/array.length);
 
-    if(modeElement===mean){
+    if(modeElement==mean){
         result=true;
         return result;
     }
-
-
     return result;
 }
 
 module.exports = modemean;
+
+
+console.log(modemean[1,2,3,3,2,1,0]);
