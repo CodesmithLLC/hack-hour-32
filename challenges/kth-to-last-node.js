2e31fa3ec9e004be25e5c249ed0e18d24a57769a
karlsonx <kiro.mm@gmail.com>
2019-10-15 09:54:02 -0700

    Done

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -8,13 +8,26 @@
  * const d = new Node('D');
  * const e = new Node('E');
  *
- * a.next = b;
- * b.next = c;
- * c.next = d;
- * d.next = e;
+ a.next = b;
+ b.next = c;
+ c.next = d;
+ d.next = e;
  *
  * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
  */
+
+//  TEST CONDITION BEGINS
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
 
 function Node(val) {
   this.value = val;
@@ -22,7 +35,16 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+let resultArray = [];
+let currentNode = head.next;
+while (currentNode) {
+  // console.log(currentNode)
+  resultArray.push(currentNode.value)
+  currentNode = currentNode.next
 }
+ return resultArray[resultArray.length - k]
+}
+
+console.log(kthToLastNode(2,a))
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
