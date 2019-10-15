41586319a703fc0ebe40557e19a97f865bcccfcc
Jason Yu <jyu.json@gmail.com>
2019-10-15 09:39:15 -0700

    kth-to-last-node-attempt-1

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,8 +21,52 @@ function Node(val) {
   this.next = null;
 }
 
-function kthToLastNode(k, head) {
+// function LinkedList() {
+//   this.head = null;
+//   this.next = null;
+// }
 
+
+function kthToLastNode(k, head) {
+  // assign the passed in head arg and assign it to this.head to know where to start
+  let curNode = head;
+  
+  // iterate through the whole list, keeping a count of how many nodes exist
+  let nodeCount = 1;
+  while (curNode.next !== null) {
+    curNode.index = 0;
+    nodeCount++
+    curNode = curNode.next;
+  }
+  // the nodeCount minus k will point to the index of the node that is specified
+  let specifiedNodeIndex = nodeCount - k;
+
+  let findIt = head;
+  while (findIt !== null) {
+    if (findIt.index === specifiedNodeIndex) {
+      return findIt.value;
+    }
+    findIt = findIt.next;
+  }
 }
+  
+
+
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+
+// const a = new Node('A');
+// const b = new Node('B');
+// const c = new Node('C');
+// const d = new Node('D');
+// const e = new Node('E');
+
+// const list = new LinkedList;
+// list.add(a);
+// list.add(b);
+// list.add(c);
+// list.add(d);
+// list.add(e);
+// console.log(list);
+
+kthToLastNode(2, a); // should return 'D'
