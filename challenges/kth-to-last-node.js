d42e0736c249fc8e13ba3c3604691f6718aefd46
Jason Yu <jyu.json@gmail.com>
2019-10-15 09:53:59 -0700

    attempt-2

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,31 +21,30 @@ function Node(val) {
   this.next = null;
 }
 
-// function LinkedList() {
-//   this.head = null;
-//   this.next = null;
-// }
-
-
 function kthToLastNode(k, head) {
   // assign the passed in head arg and assign it to this.head to know where to start
   let curNode = head;
-  
   // iterate through the whole list, keeping a count of how many nodes exist
   let nodeCount = 1;
   while (curNode.next !== null) {
-    curNode.index = 0;
     nodeCount++
     curNode = curNode.next;
   }
   // the nodeCount minus k will point to the index of the node that is specified
   let specifiedNodeIndex = nodeCount - k;
-
+  console.log(specifiedNodeIndex);
+  
+  // iterate through the node again, counting each step; 
+  // once the step count matches the specifiedNodeIndex, you are at the specified node; return its value
   let findIt = head;
+  let i = 0;
   while (findIt !== null) {
-    if (findIt.index === specifiedNodeIndex) {
+    if (i === specifiedNodeIndex) {
+      // console.log(i)
+      // console.log(specifiedNodeIndex)
       return findIt.value;
     }
+    i++
     findIt = findIt.next;
   }
 }
@@ -55,18 +54,17 @@ function kthToLastNode(k, head) {
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
 
-// const a = new Node('A');
-// const b = new Node('B');
-// const c = new Node('C');
-// const d = new Node('D');
-// const e = new Node('E');
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
 
-// const list = new LinkedList;
-// list.add(a);
-// list.add(b);
-// list.add(c);
-// list.add(d);
-// list.add(e);
-// console.log(list);
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
 
-kthToLastNode(2, a); // should return 'D'
+console.log(a);
+
+console.log(kthToLastNode(2, a)); // should return 'D'
