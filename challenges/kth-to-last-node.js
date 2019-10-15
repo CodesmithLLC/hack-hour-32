5dd821ed0f5af119bef09425c86bc3a55d4fbb6a
JoshuaManto <joshuamanto@yahoo.com>
2019-10-15 09:33:01 -0700

    Finished kth to the last node challenge. 2 solutions - 2 loop and cache versions

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,58 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  // 2 Loop version
+  //
+  let length = 0;
+  let nodeRunner = head;
+  if (k <= 0 || !k || !head) return undefined;
+  while (nodeRunner !== null) {
+    length++;
+    nodeRunner = nodeRunner.next;
+  }
+  // console.log(length);
+  const kthPoint = length - k;
+  let resultValue;
+  nodeRunner = head;
+  for (let i = 0; i <= kthPoint; i++) {
+    if (i === kthPoint) resultValue = nodeRunner.value;
+    nodeRunner = nodeRunner.next;
+  }
+  return resultValue;
+  //
+  // Cache version
+  //
+  // const cache = {};
+  // let nodeRunner = head;
+  // let length = 0;
+  // if (k <= 0 || !k || !head) return undefined;
+  // while (nodeRunner !== null) {
+  //   cache[length] = nodeRunner.value;
+  //   nodeRunner = nodeRunner.next;
+  //   length++;
+  // }
+  // return cache[length - k];
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(kthToLastNode(2, a));
+console.log(kthToLastNode(3, a));
+console.log(kthToLastNode(1, a));
+console.log(kthToLastNode(4, a));
+console.log(kthToLastNode(5, a));
+console.log(kthToLastNode(0, a));
+console.log(kthToLastNode(8, a));
+console.log(kthToLastNode(a));
+console.log(kthToLastNode());
+
+module.exports = { Node: Node, kthToLastNode: kthToLastNode };
