512c884b521538ada6c8de179183d7c41186aec7
Nobuhide Ajito <najito@Emis-MacBook.local>
2019-10-16 09:24:38 -0700

    all cases but if base was 0 and power was negative

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,17 @@
  */
 
 function pow(base, power) {
-
+    if (power === 0) {
+        return 1
+    }
+    if (power === undefined) {
+        return base
+    }
+    if (power > 0) {
+        return base * pow(base, power - 1)
+    } else {
+        return 1/base * pow(base, power + 1)
+    }
 }
 
 module.exports = pow;
