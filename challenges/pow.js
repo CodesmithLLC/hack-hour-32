2ba698dca64ee70e8cbfa980b076c67e225c933c
bmwoolf <bradleymwoolf@gmail.com>
2019-10-16 09:26:06 -0700

    attempt 1 at creating pow with recursion

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,11 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
+function pow(base, power, tracker = 1) {
+    if (power === 0) {
+        return tracker;
+    }
+    return pow(base, power -= 1, tracker *= base); //increase the base while decreasing the power
 }
 
 module.exports = pow;
