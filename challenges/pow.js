5a572be5183fe097115ffa1afe3efda48a80c333
JoshuaManto <joshuamanto@yahoo.com>
2019-10-16 09:43:46 -0700

    Power recursion, tail recursion (?), iterative, and magic done

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,39 @@
  * Use recursion!
  */
 
-function pow(base, power) {
+function pow(base, power, result = 1) {
+  //
+  //  Recursion
+  //
 
+  // if (power === 1) return base;
+  // else return pow(base, power - 1) * base;
+
+  //
+  //  Recursion (Tail??)
+  //
+  if (power === 1) return result * base;
+  else return pow(base, power - 1, result * base);
+
+  //
+  //  Iterative
+  //
+  // let pow = base;
+  // for (let i = 1; i < power; i++) {
+  //   pow *= base;
+  // }
+  // return pow;
+
+  //
+  // Magic
+  //
+  // return base**power
 }
+
+console.log(pow(3, 3));
+console.log(pow(4, 3));
+console.log(pow(10, 2));
+console.log(pow(10, 3));
+console.log(pow(2, 8));
 
 module.exports = pow;
