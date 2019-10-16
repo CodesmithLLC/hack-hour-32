901b0a2a1958bc8952f55633270690ae8132bfe1
Andrew Shin <andyrpg@gmail.com>
2019-10-16 09:28:23 -0700

    finished pow and kh to the last node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -20,9 +20,26 @@ function Node(val) {
   this.value = val;
   this.next = null;
 }
+//k is the interger from the end of the list. We want the value of it
+//head refers to the first node
+// a single list is a object of objects that points to each other using the "this.next"
+//How do we find the node we are in is the last? this.next === null
+//we would need to tranverse the nodes: using recursion?
+function kthToLastNode(k, head, count = 0) {
+    let position = head;
+    let length = 1;
+    while (position.next) {
+        position = position.next;
+        length++;
+    }
 
-function kthToLastNode(k, head) {
+    let incrementedCount = count++;
 
+    if (length === k) {
+        return head.value;
+    } else {
+        return kthToLastNode(k, head.next, incrementedCount)
+    }
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
