2c453d40eefdc4ccd4b8c57315ed28562bf2456c
evanking11 <evanking11@gmail.com>
2019-10-16 12:15:04 -0700

    pow completed

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,11 +3,8 @@
  */
 
 function pow(base, power) {
-  if (power === 0) {
-    return 1;
-  } else return base * pow(base, power - 1);
+  if (power === 0) return 1;
+  return base * pow(base, power - 1);
 }
-
-console.log(pow(3, 4));
 
 module.exports = pow;
