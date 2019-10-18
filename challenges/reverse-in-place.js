ff7e22f54ba9289fa82d1ac642e2b1ac8ab7e5cd
zerodegreekelvin <zerodegreekelvin@gmail.com>
2019-10-18 09:29:22 -0700

    reverse in place

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,12 @@
  */
 
 function reverseInPlace(array) {
-
+for(let i = 0; i <Math.ceil(array.length/2);i++){
+    let temp = array[array.length-1-i];
+    array[array.length-1-i] = array[i];
+    array[i] = temp;
+}
+return array;
 }
 
 module.exports = reverseInPlace;
