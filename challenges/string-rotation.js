7ad1944603182ad39a50814c3b98a342ce771d7d
bmwoolf <bradleymwoolf@gmail.com>
2019-10-19 09:48:53 -0700

    two solutions: one for loop that utilizes slice to bring a letter to the front on each iteration, the second being doubling the string

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,30 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
+//keep shifting until it equals the shifted string?
+//if it matches return true
+//else return false?
+  if (!s1 || !s2) {
+    return false;
+  } 
+  // for (let i = 0; i < s1.length; i++) {
+  //   let lastLetter = s2.slice(s2.length - 1);
+  //   s2 = s2.slice(0, -1) //this is what moves the string around
+  //   s2 = lastLetter + s2; //put the letter back in
 
+  //   if (s2.includes(s1)) {return true};
+  // }
+  // return false;
+
+  if (s1.length === s2.length) {
+    return (s1 + s1).includes(s2);
+  }
+  return false;
 }
+
+console.log(stringRotation("hello", "hello")) //true
+console.log(stringRotation("hello", "llohe")) //true
+console.log(stringRotation("hello", "he")) // false
+console.log(stringRotation("hello", "ollhe")) //false
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
