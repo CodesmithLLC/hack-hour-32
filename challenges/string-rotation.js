77f75de527fd638e2514031ecc87b148222b46ac
aburkett1 <austin_osten@hotmail.com>
2019-10-19 10:09:22 -0700

    refactored string-rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,21 +16,26 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
-  if (s1.length !== s2.length) return false;
-  if (s1 === s2) return true;
-  for (let i = 0; i < s2.length; i++) {
-    if (s1[0] === s2[i]) {
-      let tempArr = s2.split('');
-      while (s1[0] !== tempArr[0]) {
-        tempArr.push(tempArr[0]);
-        tempArr.shift();
-      }
-      s2 = tempArr.join('');
-      if (s1 === s2) {
-        return true;
-      }
-    }
-  }
+  // if (s1.length !== s2.length) return false;
+  // if (s1 === s2) return true;
+  // for (let i = 0; i < s2.length; i++) {
+  //   if (s1[0] === s2[i]) {
+  //     let tempArr = s2.split('');
+  //     while (s1[0] !== tempArr[0]) {
+  //       tempArr.push(tempArr[0]);
+  //       tempArr.shift();
+  //     }
+  //     s2 = tempArr.join('');
+  //     if (s1 === s2) {
+  //       return true;
+  //     }
+  //   }
+  // }
+  // return false;
+
+
+  let s1Concat = s1 + s1;
+  if (isSubstring(s1Concat, s2) && s1.length === s2.length) return true;
   return false;
 }
 
