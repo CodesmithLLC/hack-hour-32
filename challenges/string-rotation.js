03e0d39497a8f66a1d5935ded673833ca62ff3e7
Justin <humbleyetconfidentdeveloper@gmail.com>
2019-10-19 11:09:31 -0700

    str-rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,30 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
-
+  let firstCharOfS1 = 0;
+  const rotatedS2 = [];
+  if(s1.length !== s2.length) return false;
+  
+  for (let i = 0; i < s2.length; i++) {
+    if(s2[i] === s1[0]) {
+      firstCharOfS1 = i;
+      break;
+    }
+  }
+ 
+  for (let j = firstCharOfS1; j < s2.length; j++) {
+    rotatedS2.push(s2[j]);
+  }
+  
+  for (let k = 0; k < firstCharOfS1; k++) {
+    rotatedS2.push(s2[k]);
+  }
+  
+  if(s1 === rotatedS2.join('')) {
+    return true;
+  }
+  
+  return isSubstring(s1,rotatedS2.join(''));
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
