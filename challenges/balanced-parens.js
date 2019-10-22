3d816a782cb70a8539b7c45d50f8943df9208cd4
aburkett1 <austin_osten@hotmail.com>
2019-10-22 11:00:55 -0700

    Finished balanced parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,46 @@
  *
  */
 
-function balancedParens(input){
 
+
+
+function balancedParens(input){
+    const cache = [];
+    const inputArray = input.split('');
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] === '(' || inputArray[i] === '[' || inputArray[i] === '{') {
+            cache.push(inputArray[i]);
+        } else if (inputArray[i] === ')' || inputArray[i] === ']' || inputArray[i] === '}') {
+            for (let j = cache.length; j > 0; j--) {
+                if (cache[j - 1] === '(' && inputArray[i] === ')') {
+                    cache[j - 1] += inputArray[i];
+                } else if (cache[j - 1] === '[' && inputArray[i] === ']') {
+                    cache[j - 1] += inputArray[i];
+                } else if (cache[j - 1] === '{' && inputArray[i] === '}') {
+                    cache[j - 1] += inputArray[i];
+                }
+            }
+        }
+            console.log(cache);
+    }
+    for (let i = 0; i < cache.length; i++) {
+        if (cache[i] !== '()' && cache[i] !== '[]' && cache[i] !== '{}') {
+            return false;
+        }
+    }
+    return true;
 }
+
+
+
+
+console.log('Should be false, actual is >> ', balancedParens('('));
+console.log('Should be true, actual is >> ', balancedParens('()'));
+console.log('Should be false, actual is >> ', balancedParens(')('));
+console.log('Should be true, actual is >> ', balancedParens('(())'));
+
+console.log('Should be true, actual is >> ', balancedParens('[](){}'));
+console.log('Should be true, actual is >> ', balancedParens('[({})]'));
+console.log('Should be false, actual is >> ', balancedParens('[(]{)}'));
 
 module.exports = balancedParens;
