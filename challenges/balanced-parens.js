f8a10a8a527ae05f7f3d7cd4b58087072ab5deb4
bmwoolf <bradleymwoolf@gmail.com>
2019-10-22 09:30:58 -0700

    attempt 1 using a for loop, stack, and object to compare

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,32 @@
  */
 
 function balancedParens(input){
-
+    let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    for (let i = 0; i < input.length; i++) {
+        //if it has an opening brace, add it to the stack
+        if (input[i] === '(' || input[i] === '{' || input[i] === '[') {
+            stack.push(input[i]);
+        }
+        //if it has a closing brace, remove it from the stack
+        else {
+            let closing = stack.pop();
+            //match with partner in 'map'
+            if (input[i] !== map[closing]) {
+                return false;
+            }
+        }
+    }
+    if (stack.length !== 0) {
+        return false;
+    } return true;
 }
 
 module.exports = balancedParens;
+console.log(balancedParens('[](){}')); // true
+console.log(balancedParens('[({})]'));   // true
+console.log(balancedParens('[(]{)}')); // false
