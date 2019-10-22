15a480c890c25bcb2e29b3793ff8c6350f3a1352
Justin <humbleyetconfidentdeveloper@gmail.com>
2019-10-22 09:58:08 -0700

    HH-balanced-parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,27 @@
  */
 
 function balancedParens(input){
-
+    const obj = {};
+    let openning = 0;
+    let closing = 0;
+    for (let i = 0; i < input.length; i++) {
+        if (obj[input[i]] === undefined && input[i] === '(' || input[i] === '[' || input[i] === '{') {
+            obj[input[i]] = openning;
+            obj[input[i]]++;
+        } else if (obj[input[i]] === undefined && input[i] === ')' || input[i] === ']' || input[i] === '}') {
+            obj[input[i]] = closing;
+            obj[input[i]]++;
+        }
+    } 
+    // first letter of input should be openning bracket
+    if (input[0] === '(' || input[0] === '[' || input[0] === '{') {
+        for (let key in obj) {
+            if (obj['('] === obj[')'] || obj['['] === obj[']'] || obj['{'] === obj['}']) return true
+        }
+    }
+    return false;
 }
+
+console.log(balancedParens(''))
 
 module.exports = balancedParens;
