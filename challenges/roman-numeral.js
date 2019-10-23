f179219240bbdebbce96ba9a0438ee40dbbd9cc7
timjj92 <timjj92@gmail.com>
2019-10-23 09:33:47 -0700

    roman numeral attempt 1

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,31 @@
  */
 
 function romanNumeral(n) {
-
+    let obj = {
+        1: 'I',
+        4: 'IV',
+        5: 'V',
+        9: 'IX',
+        10: 'X',
+        40: 'XL',
+        50: 'L',
+        90: 'XC',
+        100: 'C',
+        400: 'CD',
+        500: 'D',
+        900: 'CM',
+        1000: 'M' 
+    }
+    let remainder = n;
+    let keys = Object.keys(obj);
+    let returnStr = "";
+    for(let i = keys.length - 1; i >= 0; i--){
+        for(let j = 1; j <= remainder/(keys[i]); j++){
+            returnStr += obj[keys[i]];
+        }
+        remainder %= keys[i];
+    }
+    return returnStr;
 }
 
 module.exports = romanNumeral;
