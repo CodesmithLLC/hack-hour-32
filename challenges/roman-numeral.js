d0f71cc5be12158641d7d7d4c25935cbde9316d8
Nobuhide Ajito <najito@Emis-MacBook.local>
2019-10-23 09:34:56 -0700

    roman-numeral completed

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,6 +18,17 @@
  */
 
 function romanNumeral(n) {
+    const m = ["", "M", "MM","MMM"];
+    const c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    const i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]
+
+    let thousands = m[Math.floor(n / 1000)];
+    let hundreds = c[Math.floor((n % 1000)/ 100)];
+    let tens = x[Math.floor((n % 100) / 10)];
+    let ones = i[Math.floor((n % 10))] || "";
+
+    return thousands + hundreds + tens + ones
 
 }
 
