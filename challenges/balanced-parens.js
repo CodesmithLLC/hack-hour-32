803eb9dca1b5c53183668dfbb3ce7f5a268df331
Jason Yu <jyu.json@gmail.com>
2019-10-22 18:51:46 -0700

    attempt 1

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,67 @@
  */
 
 function balancedParens(input){
+    const paranPairs = {
+        "(": ")",
+        "[": "]",
+        "{": "}"
+    }
+   
+    let filteredInput = input.replace("[^(){}[]]", "");
+    console.log(filteredInput);
 
+    if (filteredInput.length % 2 === 1) return false;
+    // step 1
+    // if (inputSplit[0] === "(" && inputSplit.endsWith(")")) return true;
+    // return false;
+
+    // step 2
+    let howManyCorrectPairsNeeded = filteredInput.length / 2;
+    console.log(howManyCorrectPairsNeeded);
+    // first check if their are valid pairs within the str
+    let i = 0;
+    let count = 0;
+    for (let key in paranPairs) {
+        console.log(key);
+        while (i < filteredInput.length / 2) {
+            console.log(i)
+            console.log(i + 1)
+            console.log(filteredInput[i])
+            console.log(filteredInput[i + 1])
+            if (filteredInput[i] === key && filteredInput[i + 1] === paranPairs[key]) {
+                count++;
+                console.log(count);
+                i += 2;
+                console.log(i);
+                break
+            } if (filteredInput[i] === key && filteredInput[filteredInput.length - 1] === paranPairs[key]) {
+                count++;
+                console.log(count);
+                i++;
+                console.log(i);
+            } else {
+                break;
+            }
+        }
+    }
+    
+    console.log(howManyCorrectPairsNeeded);
+    console.log(count);
+
+    if (count === howManyCorrectPairsNeeded) return true;
+    else return false;
 }
+
+// console.log(balancedParens('('));  // false
+// console.log(balancedParens('()')); // true
+// console.log(balancedParens(')('));  // false
+// console.log(balancedParens('(())'));  // true
+
+console.log(balancedParens('[](){}')); // true
+// console.log(balancedParens('[({})]')); // true
+// console.log(balancedParens('[(]{)}')); // false
+
+// console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
+// console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
 
 module.exports = balancedParens;
