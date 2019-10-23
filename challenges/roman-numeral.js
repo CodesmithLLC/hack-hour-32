4875404593a4819a997fc37ec92144fd88dbd1f6
Travis Clark <travis6clark@gmail.com>
2019-10-23 09:50:21 -0700

    roman numeral convert

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,28 @@
  * 
  */
 
-function romanNumeral(n) {
-
+function romanNumeral(n, output) {
+    let roman = ''
+    if(output){ roman = output };
+    if(n - 10000 >= 0){ roman += '(X)'; return romanNumeral(n - 10000, roman) }
+    else if(n - 9000 >= 0){ roman += 'M(X)'; return romanNumeral(n - 9000, roman) }
+    else if(n - 5000 >= 0){ roman += '(V)'; return romanNumeral(n - 5000, roman) }
+    else if(n - 1000 >= 0){ roman += 'M'; return romanNumeral(n - 1000, roman) }
+    else if(n - 900 >= 0){ roman += 'CM'; return romanNumeral(n - 900, roman) }
+    else if(n - 500 >= 0){ roman += 'D'; return romanNumeral(n - 500, roman) }
+    else if(n - 400 >= 0){ roman += 'CD'; return romanNumeral(n - 400, roman) }
+    else if(n - 100 >= 0){ roman += 'C'; return romanNumeral(n - 100, roman) }
+    else if(n - 90 >= 0){ roman += 'XC'; return romanNumeral(n - 90, roman) }
+    else if(n - 50 >= 0){ roman += 'L'; return romanNumeral(n - 50, roman) }
+    else if(n - 40 >= 0){ roman += 'XL'; return romanNumeral(n - 40, roman) }
+    else if(n - 10 >= 0){ roman += 'X'; return romanNumeral(n - 10, roman) }
+    else if(n - 9 >= 0){ roman += 'IX'; return romanNumeral(n - 9, roman) }
+    else if(n - 5 >= 0){ roman += 'V'; return romanNumeral(n - 5, roman) }
+    else if(n - 4 >= 0){ roman += 'IV'; return romanNumeral(n - 4, roman) }
+    else if(n - 1 >= 0){ roman += 'I'; return romanNumeral(n - 1, roman) }
+    else{ return roman };
 }
+
+console.log(romanNumeral(30049))
 
 module.exports = romanNumeral;
