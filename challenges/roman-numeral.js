99ff1b730f4e91490a42a93b95a99ce4b17a7cf1
EngineerJustin <jrayjoseph@gmail.com>
2019-10-23 11:33:01 -0700

    Completed Roman Numeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,40 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
-function romanNumeral(n) {
-
+function romanNumeral(n, output = '') {
+    if (n === 0) return output;
+    if (n >= 1000) {
+        return romanNumeral(n - 1000, output += 'M');
+    } else if (n >= 900) {
+        return romanNumeral(n - 900, output += 'CM');
+    } else if (n >= 500) {
+        return romanNumeral(n - 500, output += 'D');
+    } else if (n >= 400) {
+        return romanNumeral(n - 400, output += 'CD');
+    } else if (n >= 100) {
+        return romanNumeral(n - 100, output += 'C');
+    } else if (n >= 90) {
+        return romanNumeral(n - 90, output += 'XC');
+    } else if (n >= 50) {
+        return romanNumeral(n - 50, output += 'L');
+    } else if (n >= 40) {
+        return romanNumeral(n - 40, output += 'XL');
+    } else if (n >= 10) {
+        return romanNumeral(n - 10, output += 'X');
+    } else if (n >= 9) {
+        return romanNumeral(n - 9, output += 'IX');
+    } else if (n >= 5) {
+        return romanNumeral(n - 5, output += 'V');
+    } else if (n >= 4) {
+        return romanNumeral(n - 4, output += 'IV');
+    } else if (n >= 1) {
+        return romanNumeral(n - 1, output += 'I');
+    }
 }
+
+console.log(romanNumeral(1999))
 
 module.exports = romanNumeral;
