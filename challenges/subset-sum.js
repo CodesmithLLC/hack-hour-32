056276a78a2cb9d2758dfe058a53584b01a9708e
karlsonx <kiro.mm@gmail.com>
2019-10-25 11:22:40 -0700

    almost done

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -8,8 +8,39 @@
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
 
-function subsetSum(array, target) {
+// function subsetSum(array, target) {
+// let tempSum = 0;
+//     for (let i = 0; i<array.length; i++) {
+//        if(array[i]=== target) {
+//         return true;
+//        } else if (array[i] > target) {
+//         //    console.log('I\'m way bigger, dude')
+//     } else if (array[i] < target) {
+//        tempSum+=array[i];
+//     //    console.log('Tempsum is', tempSum)
+//        for (let j = 1; j<array.length; j++) {
+//            if (array[i]+array[j]===target) {
+//                return true
+//            } else if (array[i]+array[j]>target) {
+//             console.log('Nope, I am bigger again')
+//            } else if (array[i]+array[j]>target) {
+//             tempSum+=array[j];
+//            }
+//        }//end of j loop
+//     }
+//  }//end of i for loop
+// }//end of func
 
-}
+function subsetSum(array, target) {
+    let tempSum = 0;
+    for (let i = 0; i<array.length; i++) {
+        if(array[i]=== target || tempSum === target) {
+        return true;
+    } else if (array[i]<target){
+        tempSum+=array[i];
+        
+    } 
+
+console.log(subsetSum([8,-2, 1, -3], 6))
 
 module.exports = subsetSum;
