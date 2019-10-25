ae423c3a960792368abeef945d640a27e4b621bc
Jason Yu <jyu.json@gmail.com>
2019-10-25 09:47:31 -0700

    subset sum recursively

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -9,7 +9,18 @@
  */
 
 function subsetSum(array, target) {
+    if (target === 0) return true;
+    if (array.length === 0) return false;
 
+    const subtractNumFromTarget = (subsetSum(array.slice(1), target - array[0]));
+    const nextSubsetArr = (subsetSum(array.slice(1), target));
+
+    return subtractNumFromTarget || nextSubsetArr;
 }
 
 module.exports = subsetSum;
+
+// console.log(subsetSum([3, 7, 4, 2], 5)); // true
+// console.log(subsetSum([3, 34, 4, 12, 5, 12], 32)); // true
+// console.log(subsetSum([8, 2, 4, 12], 13)) // false
+// console.log(subsetSum([8, -2, 1, -3], 6)) // true
