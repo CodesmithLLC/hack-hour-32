005677367cbdf65f59caa5eee4290bcfb3ba2ab7
Jason Yu <jyu.json@gmail.com>
2019-10-26 10:01:11 -0700

    bin-to-dec-while-loop

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,36 @@
  */
 
 function binToDec(binary) {
+    // binary numbers are just base 2 instead of base 10
+    // each digits place is +1 to the exponent on base 2 (2^0, 2^1, 2^2, and on)
+    // 100 -> 1x2x2 + 0x2 + 0x1 = 4
 
+    // first remove any heading 0's from beginning of string
+    const frontZeroesRemoved = binary.replace(/^0+/, '');
+    let number = 0;
+
+    let i = 0;
+    let exponent = frontZeroesRemoved.length - 1;
+    
+    // future refactoring: work right to left; check the curVal, if there's a 1, multiply it by 2 ^ power of the index
+    while (i < frontZeroesRemoved.length) {
+        number += frontZeroesRemoved[i] * (2 ** exponent);
+        i++;
+        exponent--;
+    }
+
+    // let arr = [...frontZeroesRemoved];
+
+    // const number = arr.reduceRight((acc, curVal) => {
+    //     acc * (2 ** )   
+    // })
+
+    return number;
 }
 
+
+
 module.exports = binToDec;
+
+console.log(binToDec('0101')); // return 5
+console.log(binToDec('1111')); // return 15
