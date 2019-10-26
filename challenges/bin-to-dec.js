78f71ba0bff32cfcef9ad264b3c27c8c258185ce
Scott McInnis <scottalyst@gmail.com>
2019-10-26 09:28:43 -0700

    Hack Hour: Bin to Dec, Attempt I: counting backwards

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -13,8 +13,25 @@
  * Write a function that converts a decimal number to binary (then maybe hexadecimal)
  */
 
-function binToDec(binary) {
+ // length - i			5 - 3 = 2
+ // length: 5			01011
+ // i = 3				   ^
 
+
+function binToDec(binary) {
+	let length = binary.length
+  let sum = 0;
+
+  for(let i = 0; i < length - 1; i += 1) {
+    if (binary[i] === '1') {
+      sum += Math.pow(2, length - 1 - i)
+    }
+  }
+  if (binary[length - 1] === '1') {
+    sum += 1;
+  }
+
+  return sum;
 }
 
 module.exports = binToDec;
