1faf85c1bc2bc9ae8d7546246b0d6e7254f9cf1e
aburkett1 <austin_osten@hotmail.com>
2019-10-29 11:19:50 -0700

    Completed match-word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -11,7 +11,44 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
-
+    str = str.toLowerCase()
+    const alph = new Set (['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
+    let word = '';
+    const wordsArr = [];
+    const stack = [];
+    for (let i = 0; i < str.length; i++) {
+        if (alph.has(str[i])) {
+            word += str[i];
+        } else {
+            if (word !== '') {
+                wordsArr.push(word);
+                word = '';
+            }
+        }
+    }
+    if (word !== '') {
+        wordsArr.push(word);
+    }
+    for (let i = 0; i < wordsArr.length; i++) {
+        if (stack[stack.length - 1] === wordsArr[i].split('').reverse().join('')) {
+            stack.pop()
+        } else {
+            stack.push(wordsArr[i]);
+        }
+    }
+    if (stack[0] === undefined) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+
+console.log(matchWord('__END_DNE-----'));
+console.log(matchWord('__ENDDNE__'));
+console.log(matchWord('IF()()fi[]'));
+console.log(matchWord('for__if__rof__fi'));
+console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));
+// console.log(matchWord(''));
 
 module.exports = matchWord;
