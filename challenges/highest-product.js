b66450e799aa82445a0cb420892ac819c0773f45
JoshuaManto <joshuamanto@yahoo.com>
2019-10-30 09:55:14 -0700

    Refactored for comparing the highest all positive product and the highest partial negative and positive product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -6,19 +6,22 @@ function highestProduct(array) {
   const tempArray = array.sort((a, b) => a - b);
   console.log(tempArray);
 
-  let product = 1;
+  let product1 = 1;
+  let product2 = 1;
   if (tempArray[0] < 0 && tempArray[1] < 0)
-    product = tempArray[0] * tempArray[1] * tempArray[tempArray.length - 1];
-  else {
-    for (let i = tempArray.length - 1; i > tempArray.length - 1 - 3; i--) {
-      product *= tempArray[i];
-    }
+    product1 = tempArray[0] * tempArray[1] * tempArray[tempArray.length - 1];
+
+  for (let i = tempArray.length - 1; i > tempArray.length - 1 - 3; i--) {
+    product2 *= tempArray[i];
   }
 
-  return product;
+  if (product1 > product2) return product1;
+
+  return product2;
 }
 
 console.log(highestProduct([5, 10, 2, 19, 1, 11]));
 console.log(highestProduct([5, 10, -2, 19, -1, 11]));
+console.log(highestProduct([0, -2, 1, -1, 11]));
 
 module.exports = highestProduct;
