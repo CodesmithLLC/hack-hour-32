b36e6239c17db645f6b9536bdabaa194a2b6f1e1
EngineerJustin <jrayjoseph@gmail.com>
2019-10-30 09:22:06 -0700

    completed highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,16 @@
  */
 
 function highestProduct(array) {
-
+    const products = [];
+    for (let i = 0; i < array.length; i += 1) {
+        for (let j = i + 1; j < array.length; j += 1) {
+            products.push(array[i] * array[j]);
+        }
+    }
+    return Math.max(...products);
 }
 
+
+console.log(highestProduct([2, 3, 4, 5, 6, 7]))
 
 module.exports = highestProduct;
