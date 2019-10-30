b6826c6896c00a01cdc07cac980f7dc9f5c76ab6
bmwoolf <bradleymwoolf@gmail.com>
2019-10-30 09:45:29 -0700

    attempted with recursion/reduce/closure, but had issues with invocation. Refactored with reverse sort.

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,44 @@
  */
 
 function highestProduct(array) {
-
+    let sorted = array.sort().reverse();
+    let product = sorted[0] * sorted[1] * sorted[2];
+    return product;
 }
 
+// function highestProduct(array) {
+//     //create copy array
+//     let copy = [...array];
+//     //array of numbers for summation
+//     let sumArr = [];
+//     //we need to call this multiple times, so we need a count
+//     let count = 1;
+//     function innerFunc() {
+//         if (count > 3) {
+//             return copy.reduce((acc, cur) => {
+//                 acc *= cur;
+//                 return acc;
+//             })
+//         } else {
+//             //loop through to find all three
+//         for (let i = 0; i < copy.length; i++) {
+//             //find largest
+//             largest = Math.max.apply(Math, array);
+//             console.log(`largest on iteration: ${largest}`)
+//             //push it to the new storage array
+//             sumArr.push(copy[i]);
+//             console.log(`sumArr at iteration: ${sumArr}`)
+//             //remove it from the copy array
+//             copy.splice(i,1)
+//             console.log(`copy after splice: ${copy}`)
+//             count++;
+
+//             innerFunc()
+//         }
+//         }
+//         }
+// }
+
+// console.log(highestProduct([1,3,5,2,6,4]));
 
 module.exports = highestProduct;
