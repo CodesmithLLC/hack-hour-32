92a9567f0407b31d49219d537a53908b930fd259
Jason Yu <jyu.json@gmail.com>
2019-10-30 10:15:44 -0700

    updated first attempt

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -6,18 +6,21 @@ function highestProduct(array) {
     if (array.length < 3) return 0;
     if (Array.isArray(array) === false) return 0;
 
-    let smallestTwoNums = array.sort((a, b) => a > b).slice(0, 3);
-    console.log(smallestTwoNums)
-    let largestTwoNums = array.sort((a, b) => a < b).slice(0, 3);
-    console.log(largestTwoNums)
+    const smallestTwoNums = array.sort((a, b) => a > b).slice(0, 2);
+    const largestThreeNums = array.sort((a, b) => a < b).slice(0, 3);
 
-    if (Math.abs(smallestTwoNums[0] * smallestTwoNums[1]) > Math.abs(largestTwoNums[0] * largestTwoNums[1])) {
-        return array[0] * array[1] * array[array.length -1]
-    } else {
-        return largestTwoNums[0] * largestTwoNums[1] * largestTwoNums[2]
-    }
+    const accountingNegNums = smallestTwoNums[0] * smallestTwoNums[1] * largestThreeNums[0];
+    const largestPosNums = largestThreeNums[0] * largestThreeNums[1] * largestThreeNums[2];
+
+    return Math.max(accountingNegNums, largestPosNums);
+
+    // if (Math.abs(smallestTwoNums[0] * smallestTwoNums[1]) > largestThreeNums[0] * largestThreeNums[1]) {
+    //     return smallestTwoNums[0] * smallestTwoNums[1] * largestThreeNums[0]
+    // } else {
+    //     return largestThreeNums[0] * largestThreeNums[1] * largestThreeNums[2]
+    // }
 }
 
-// console.log(highestProduct([0, 1, 9, 2, 40, 32, -2, 7, -100, -1001]));
+console.log(highestProduct([0, 1, 9, 2, 40, 32, -2, 7, -100, -1001]));
 
 module.exports = highestProduct;
