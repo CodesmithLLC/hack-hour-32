8509b54e8ce8876355968b2ba9addee901acd91c
JoshuaManto <joshuamanto@yahoo.com>
2019-10-30 09:26:09 -0700

    Sorted array then got the product of the 3 highest in the array

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,17 @@
  */
 
 function highestProduct(array) {
+  const tempArray = array.sort((a, b) => a - b);
+  console.log(tempArray);
 
+  let product = 1;
+
+  for (let i = tempArray.length - 1; i > tempArray.length - 1 - 3; i--) {
+    product *= tempArray[i];
+  }
+  return product;
 }
 
+console.log(highestProduct([5, 10, 2, 19, 1, 11]));
 
 module.exports = highestProduct;
