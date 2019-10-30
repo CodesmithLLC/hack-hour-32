bdb00b487f6fe9cc24ba1bbb7425a2534c47797e
timjj92 <timjj92@gmail.com>
2019-10-30 11:27:25 -0700

    edge cases handle

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,8 @@
  */
 
 function highestProduct(array) {
-  if(array.length < 3) return 0;
   if(!Array.isArray(array)) return 0;
+  if(array.length < 3) return 0;
   let ar = array.sort((a,b) => a - b);
   return Math.max(ar[ar.length-1] * ar[ar.length-2] * ar[ar.length-3], ar[ar.length-1] * ar[0] * ar[1]);
 }
