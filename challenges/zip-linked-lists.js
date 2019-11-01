a64fed87fa1b0794fb2c7f7c4a745effe73a161a
aburkett1 <austin_osten@hotmail.com>
2019-11-01 09:54:43 -0700

    Completed zip-linked-lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,56 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  const zippedNode = new Node(l1.value);
+  // console.log(zippedNode)
+  let pointerZip = zippedNode;
+  let pointerL1 = l1.next;
+  let pointerL2 = l2;
+  // console.log(pointerL1)
+  while (pointerL1 || pointerL2) {
+    if (pointerL2) {
+      pointerZip.next = pointerL2;
+      pointerL2 = pointerL2.next;
+      pointerZip = pointerZip.next;
+    }
+    if (pointerL1) {
+      pointerZip.next = pointerL1;
+      pointerL1 = pointerL1.next;
+      pointerZip = pointerZip.next;
+    }
+  }
+  // console.log(zippedNode);
+  return zippedNode;
 };
+
+
+const link1 = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: null,
+    }
+  }
+}
+
+const link2 = {
+  value: 4,
+  next: {
+    value: 5,
+    next: {
+      value: 6,
+      next: null,
+    }
+  }
+}
+
+
+
+
+
+
+console.log(zip(link1, link2));
 
 module.exports = {Node: Node, zip: zip};
