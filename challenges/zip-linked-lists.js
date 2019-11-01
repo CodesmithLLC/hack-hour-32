4b42c9b5e886b8b3e9da993294b0b6f73ac3f39c
Nobuhide Ajito <najito@Emis-MacBook.local>
2019-11-01 09:37:06 -0700

    zip-linked lists done

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,18 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  let current1 = l1;
+  let current2 = l2;
+  let trailing1 = l1;
+  let trailing2 = l2;
+  while (current1 !== null || current2 !==null) {
+    current1 = current1.next;
+    current2 = current2.next;
+    trailing1.next = trailing2;
+    trailing2.next = current1;
+    trailing1 = current1;
+    trailing2 = current2;
+  }
 };
 
 module.exports = {Node: Node, zip: zip};
