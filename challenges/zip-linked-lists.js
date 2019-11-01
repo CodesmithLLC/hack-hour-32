2f85fed81bac43d6d4bb5e01ea1b5427e43e607e
bmwoolf <bradleymwoolf@gmail.com>
2019-11-01 10:05:48 -0700

    attempt with creating a storage linked list and returning that. not sure if it works as ran out of time for testing

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,52 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  //create new linked list to store the zipped ones- i dont think my methodology is allowing for every other one
+  let l3 = new Node()
+  let prev = l3;
+  //while they are both not null
+  while (l1 && l2) {
+    //if l1 is less than the l2value
+    if (l1.value <= l2.value) { 
+      prev.next = l1;
+      l1 =l1.next;
+    } else {
+      prev.next = l2;
+      l2 = l2.next
+    }
+    prev = prev.next;
+  }
+
+  // at the end of the list
+  if (!l1) {prev.next = l2};
+  if (!l2) {prev.next = l1};
+  //return the total new linked list
+  return l3.next;
 };
+
+
+// Node zip(l1, l2) {
+//   if (l1 == null) return l2;
+//   if (l2 == null) return l1;
+
+//   Node head;
+//   if (l1.data < l2.data) {
+//     head = l1;
+//   } else {
+//     head = l2;
+//     l2 = l1;
+//     l1 = head;
+//   }
+//   while(l1.next != null) {
+//     if (l1.next.data > l2.data) {
+//       Node tmp = l1.next;
+//       l1.next = l2;
+//       l2 = tmp;
+//     }
+//     l1 = l1.next;
+//   } 
+//   l1.next = l2;
+//   return head;
+// }
 
 module.exports = {Node: Node, zip: zip};
