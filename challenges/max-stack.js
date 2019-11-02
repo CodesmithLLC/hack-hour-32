646d13aa48139e9794d6470d9649fb54af73e8ed
Mychal Estalilla <mychal.es@protonmail.com>
2019-11-02 10:12:45 -0700

    complete max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,44 @@
 
 function Stack() {
   // body...
+  this.length = 0;
+  this.stack = {};
+  this.max = [];
 }
+
+Stack.prototype.push = function(val)  {
+  this.stack[this.length] = val;
+  this.length++;
+  this.max.push(val);
+  this.max = this.max.sort();
+
+  return this.length;
+
+}
+
+Stack.prototype.pop = function()  {
+  const popVal = this.stack[this.length-1]
+  delete this.stack[this.length-1]
+  this.length--;
+  const index = this.max.indexOf(popVal);
+  this.max.splice(index, 1);
+
+  return popVal;
+
+}
+
+Stack.prototype.getMax = function() {
+  return this.max[this.max.length-1];
+}
+
+
+let newStack = new Stack;
+
+console.log(newStack.push(0));
+console.log(newStack.push(28));
+console.log(newStack.push(21));
+console.log(newStack.push(40));
+console.log(newStack.pop());
+console.log(newStack.getMax());
 
 module.exports = Stack;
