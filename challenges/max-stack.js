3f4bd6544d6330d37c9f8618e84daafff7cdb5f2
karlsonx <kiro.mm@gmail.com>
2019-11-02 10:13:51 -0700

    max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,45 @@
 
 function Stack() {
   // body...
+  this.storage = {};
+  this.indexLength = 0;
+  this.getMax = 0;
 }
+
+Stack.prototype.push = function(value) {
+  this.storage[this.indexLength] = value;
+  // console.log(this.storage)
+  this.indexLength++;
+
+//if value is > than the other values >> assign it to get max
+if (value > this.getMax) {
+  this.getMax = value;
+  console.log('Get Max:', this.getMax)
+  } 
+
+  return this.indexLength
+}
+
+Stack.prototype.pop = function() {
+  let temp = this.storage[this.indexLength-1] 
+  delete this.storage[this.indexLength-1] 
+  if (this.getMax === temp) {
+    const maxArray = Object.values(this.storage).sort();
+    maxArray.pop();
+    this.getMax = maxArray[maxArray.length-1]
+    console.log('maxArray', maxArray, 'New get max', this.getMax)
+  }
+  this.indexLength--;
+  return temp
+}
+
+const pushTest = new Stack();
+console.log('Length',pushTest.push(4))
+console.log('Length',pushTest.push(5))
+console.log('Length',pushTest.push(6))
+console.log('Length',pushTest.push(2))
+console.log('Length',pushTest.push(1))
+console.log('Length',pushTest.push(6))
+console.log('Pop',pushTest.pop())
 
 module.exports = Stack;
