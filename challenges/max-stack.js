b1d59fabb389df1a1a682593ba198cbf8b055b82
justin1j <wonjun110@gmail.com>
2019-11-02 09:56:08 -0700

    max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,37 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = {};
+  this.length = 0;
 }
+
+Stack.prototype.push = function(...args) {
+  args.forEach(arg => this.storage[this.length++] = arg)
+  return this.length;
+}
+
+Stack.prototype.pop = function() {
+  const popped = this.storage[this.length - 1];
+  delete this.storage[this.length - 1];
+  this.length--;
+  return popped;
+}
+
+Stack.prototype.getMax = function() {
+  const obj = this.storage;
+  Object.values(obj).sort((a,b) => b - a);
+  return obj[this.length - 1]
+}
+
+var arr = new Stack()
+
+arr.push(1)
+arr.push(2)
+arr.push(3)
+console.log(arr)
+console.log(arr.getMax())
+
+arr.pop()
+console.log(arr)
 
 module.exports = Stack;
