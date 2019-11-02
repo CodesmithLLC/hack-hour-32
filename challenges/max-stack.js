4e79db37ab6e48f7fdeb1fd081b7fd073cd70d14
bmwoolf <bradleymwoolf@gmail.com>
2019-11-02 09:58:57 -0700

    attempt 1, having issues with this keyword and context

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,30 @@
 
 function Stack() {
   // body...
+  this.obj = {};
+  this.index = 0;
+
+  this.prototype.push = function (value) {
+    this.object.index ++;
+    this.object.index = value;
+    return this.object.length;
+  }
+
+  this.prototype.pop = function (value) {
+    //create a storage variable
+    let storage = value;
+    //delete that value
+    delete Math.max(this.obj.index)
+    //return storage
+    return storage;
+  }
+
+  this.prototype.getMax = function () {
+    //reference the last key 
+    let last = Object.keys(this.obj)[Object.keys(this.obj).length-1];
+    //return the last index
+    return last;
+  }
 }
 
 module.exports = Stack;
