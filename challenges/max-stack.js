a57b25a3ef23ce5eac35f7c2cf3127db1cd6cfbb
Nobuhide Ajito <najito@Emis-MacBook.local>
2019-11-02 09:39:24 -0700

    max-stack done

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,39 @@
 
 function Stack() {
   // body...
+  this.storage = {};
+  this.length = 0
+  this.max = [];
+}
+//push
+Stack.prototype.push = function(val) {
+  this.storage[this.length] = val;
+  this.length ++;
+  for (let i = 0; i < this.max.length; i ++) {
+    if (this.max[i] > val) {
+      splice(i, 0, val);
+      return this.length;
+    }
+    if (this.max[i + 1] === undefined) {
+      this.max[this.max.length] = val;
+      return this.length
+    }
+  }
+}
+//pop
+Stack.prototype.pop = function() {
+  const popped = this.storage[this.length - 1]
+  delete this.storage[this.length - 1];
+  this.length --;
+  this.max.pop();
+  return popped;
 }
 
+//getmax
+Stack.prototype.getMax = function() {
+  if (this.length === 0) {
+    return "there is no maximum value in an empty stack"
+  }
+  return this.max[this.max.length - 1]
+}
 module.exports = Stack;
