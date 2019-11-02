7852f419109c19cef5a66dd07300d30b98e3e6e2
choies921003 <choies921003@gmail.com>
2019-11-02 10:16:52 -0700

    hack hour max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,31 +7,31 @@
  */
 
 function Stack() {
-  const obj = {};
+  this.obj = {};
   this.length = 0;
   this.largestNum = 0;
 }
   //push
   Stack.prototype.push = (value) => {
-    obj[this.length] = value;
+    this.obj[this.length] = value;
     this.length += 1;
-    return obj[this.length];
+    return this.length;
   }
   //pop
   Stack.prototype.pop = () =>{
     if (this.length === 0) return undefined;
-    let lastElement = obj[this.length-1];
-    delete obj[this.length-1];
+    let lastElement = this.obj[this.length-1];
+    delete this.obj[this.length-1];
     return lastElement;
   }
   //getMax
   Stack.prototype.getMax = () =>{
     for (let i=0; i<this.length; i++){
-      if (obj[i]> largestNum){
-        largestNum = obj[i];
+      if (this.obj[i]> this.largestNum){
+        this.largestNum = this.obj[i];
       }
     }
-    return largestNum;
+    return this.largestNum;
   }
 
 module.exports = Stack;
