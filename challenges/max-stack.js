6738b18669b0c6b8e983e32a495c14e9b15212fc
EngineerJustin <jrayjoseph@gmail.com>
2019-11-02 09:57:26 -0700

    completed max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,53 @@
  */
 
 function Stack() {
-  // body...
+  let stack = {};
+  stack.index = 0;
+  stack.max = 0;
+  stack.oldMax = 0;
+  const push = (val) => {
+      const key = stack.index;
+      stack[key] = val;
+      if (key === 0) {
+          stack.index += 1;
+          stack.max = stack[key];
+          return stack.index;
+      }
+      stack.index += 1;
+      if (stack[key] > stack[key - 1]) {
+          stack.oldMax = stack.max;
+          stack.max = stack[key]
+      } else {
+          stack.oldMax = stack.max;
+          stack.max = stack[key - 1];
+      }
+      return stack.index;
+  }
+  stack.push = push;
+
+  const pop = () => {
+      const ele = stack[stack.index - 1];
+      delete stack[stack.index - 1];
+      if (stack.index !== 0) {
+          stack.index -= 1;
+      }
+      if (ele === stack.max) {
+          stack.max = stack.oldMax;
+      }
+      return ele;
+  }
+  stack.pop = pop;
+
+  const getMax = () => {
+      return stack.max;
+  }
+  stack.getMax = getMax;
+
+  return stack;
 }
+
+const test = Stack();
+
+
 
 module.exports = Stack;
