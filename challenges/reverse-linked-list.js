edffde8687500fe4c874a9d6898d72528de6ff8a
timjj92 <timjj92@gmail.com>
2019-11-05 09:40:16 -0800

    reverse linked list attempt 1

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,27 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    if(!head) return null;
+    if(head.next === null) return head;
+    let prev = head;
+    let current = head.next;
+    let next = head.next.next;
+    prev.next = null;
+    while(current !== null){
+        current.next = prev;
+        prev = current;
+        current = next; 
+        if(next !== null) next = next.next;
+    }
+    return prev;
 }
 
+// let a = new Node(1);
+// a.next = new Node(2);
+// a.next.next = new Node(3);
+// a.next.next.next = new Node(2);
+
+// for(let i = reverseLinkedList(a.next.next.next); i !== null; i = i.next){
+//     console.log(i.value);
+// }
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
