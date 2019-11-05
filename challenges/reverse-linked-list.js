ef49525a380e5ebc0b437cfc9b26a3de97a1208d
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2019-11-05 09:49:46 -0800

    ll 2 in place

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -37,15 +37,23 @@ function reverseLinkedList(head) {
 
     while(currentNode){
         nodesArray.push(currentNode);
-        currentNode=currentNoe.next;
+        currentNode=currentNode.next;
     }
+
+    console.log(nodesArray);
 
     //now iterate array and reassign the next
     head=nodesArray[nodesArray.length-1];//we can reassign head because we already stored the refs in an array of pointers
     currentNode=head; //reuse
-    for(let i=nodesArray.length-1;i>0;i--){
+    for(let i=nodesArray.length-1;i>=0;i--){
+        if(i===0){
+            console.log(currentNode);
+            currentNode.value=nodesArray[i].value;
+            currentNode.next=null;
+            break;
+        }
         currentNode=nodesArray[i]; //
-        currentNode.next=nodesArray[i].next; //going backwards rereferencings
+        currentNode.next=nodesArray[i-1]; //going backwards rereferencings
     }
 
 
@@ -55,3 +63,10 @@ function reverseLinkedList(head) {
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
+
+
+let ll=new Node(1);
+ll.next=new Node(2);
+ll.next.next=new Node(3);
+ll.next.next.next=new Node(4);
+console.log(reverseLinkedList(ll));
