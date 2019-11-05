5fafc4f1f44c70b2667fcf4d36c4f78e2c4aacdf
Nobuhide Ajito <najito@Emis-MacBook.local>
2019-11-05 09:42:24 -0800

    reverse-linked list done in place

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,31 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    let lead = head;
+    let trailing1 = head;
+    let trailing2 = head;
+    lead = lead.next;
+    trailing1 = lead
+    lead = lead.next;
+    trailing2.next = null
+    while (lead.next.next !== null || lead.next !== null) {
+        trailing1.next = trailing2;
+        trailing2 = lead;
+        lead = lead.next;
+        trailing2.next = trailing1;
+        trailing1 = lead;
+        lead = lead.next;
+    }
+    trailing1.next = trailing2;
+    if (lead.next.next === null) {
+        trailing2 = lead;
+        lead = lead.next;
+        trailing2.next = trailing1;
+        lead.next = trailing2;
+        return;
+    }
+    lead.next = trailing1;
+    return;
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
