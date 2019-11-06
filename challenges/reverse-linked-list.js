d12bf6543ca30cb72c0f17e88fb74c999b96d17d
Mychal Estalilla <mychal.es@protonmail.com>
2019-11-06 09:34:00 -0800

    refactor reverse-linked-list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,41 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+  let lead;
+  let follow = null; 
+  let curr = head;
+
+
+  while (curr) {
+	lead = curr.next;
+	curr.next = follow;
+	follow = curr;
+	curr = lead;
+  }
+
+  // Assign head to follow, which will point to the first element after loop
+  head = follow;
+
+  return head;
 
 }
+
+
+  /*
+const a = new Node(1);
+const b = new Node(2);
+const c = new Node(3);
+const d = new Node(4);
+const e = new Node(5);
+const f = new Node(6);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+
+console.log(reverseLinkedList(a));
+*/
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
