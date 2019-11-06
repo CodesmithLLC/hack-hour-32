b48ee33f6a93855b0af3f6cfdb81ba5ec45c088f
Mychal Estalilla <mychal.es@protonmail.com>
2019-11-06 09:29:02 -0800

    complete apple-stocks

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -14,6 +14,35 @@
 
 function bestProfit(stock_prices_yesterday) {
 
+  // Edge cases
+  if (stock_prices_yesterday.length < 1) return 0;
+
+  let low = Number.POSITIVE_INFINITY;
+  let high = Number.NEGATIVE_INFINITY;
+
+  let highTime;
+  let lowTime;
+
+  stock_prices_yesterday.forEach( (price, i) => {
+	// Re-assign high and low;
+	if (price < low) {
+	  low = price;
+	  lowTime = i;
+	}
+	if (price > high) {
+	  high = price;
+	  hightime = i;
+	}
+  });
+
+  if (high - low < 0) return 0;
+
+  return high - low;
+
 }
+
+//const prices = [500, 550, 400, 480, 600, 530];
+
+//console.log(bestProfit(prices));
 
 module.exports = bestProfit;
