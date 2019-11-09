581eb44e89262b223eadcfe1a2e183d4cefd3b90
EngineerJustin <jrayjoseph@gmail.com>
2019-11-09 09:45:20 -0800

    Completed Perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,40 @@
  */
 
 function permPalin(str) {
-	
+	const holding = {};
+	let odds = 0;
+    if (str.length % 2 !== 0) {
+        for (let i = 0; i < str.length; i += 1) {
+            if (holding[str[i]] === undefined) {
+                holding[str[i]] = 1;
+            } else {
+                holding[str[i]] += 1;
+            }
+        }
+        for (let prop in holding) {
+            if (holding[prop] % 2 !== 0) {
+                odds += 1;
+            }
+        }
+        if (odds !== 1) return false;
+        return true;
+    } else {
+	    for (let i = 0; i < str.length; i += 1) {
+            if (holding[str[i]] === undefined) {
+                holding[str[i]] = 1;
+            } else {
+                holding[str[i]] += 1;
+            }
+        }
+        for (let prop in holding) {
+            if (holding[prop] % 2 !== 0) {
+                odds += 1;
+            }
+        }
+        if (odds !== 0) return false;
+        return true;
+    }
 }
+
 
 module.exports = permPalin;
