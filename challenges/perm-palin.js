b837fa326e9b90f25ec3c32c2040f5e027b75953
Scott McInnis <scottalyst@gmail.com>
2019-11-09 09:27:30 -0800

    Hack Hour: PermPalin...really this simple?

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,24 @@
  */
 
 function permPalin(str) {
-	
+	if (str.length === 1) return true;
+
+	let newObj = {};
+  let oddNumbers = 0;
+
+	let stringArray = str.split('');
+	stringArray.forEach(element => {
+		if (!newObj[element]) { newObj[element] = 0; }
+		newObj[element] += 1;
+	});
+
+  for(let letter in newObj) {
+    if (newObj[letter] % 2 !== 0) {
+      oddNumbers += 1;
+    }
+  }
+
+	return oddNumbers > 1 ? false : true;
 }
 
 module.exports = permPalin;
