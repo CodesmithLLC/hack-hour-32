52217b23679f243a32f6d0424da76dd27cc2e424
Nobuhide Ajito <najito@Emis-MacBook.local>
2019-11-09 09:39:21 -0800

    perm-palin completed

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,25 @@
  */
 
 function permPalin(str) {
-	
+    const cache = {};
+    let odd = false;
+    for (let i = 0; i < str.length; i ++) {
+        if (cache[str[i]] === undefined) {
+            cache[str[i]] = 1;
+        } else {
+            cache[str[i]]++;
+        }
+    }
+    const check = Object.values(cache)
+        for (let i = 0; i < check.length; i ++) {
+          if (check[i] % 2 !==0 && odd === true) {
+              return false
+            }
+          if (check[i] % 2 !== 0 && odd === false) {
+              odd = true
+          }
+        }
+  return true;
 }
 
 module.exports = permPalin;
