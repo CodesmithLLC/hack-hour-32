6d0cede30e0e9acf1f59b0826e38d719678609bd
Mychal Estalilla <mychal.es@protonmail.com>
2019-11-09 09:32:06 -0800

    first attempt of perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,19 @@
  */
 
 function permPalin(str) {
-	
+  const rev = str.split('').reverse().join('');
+  let candidate = rev[0];
+
+  // Edge cases
+  if (str.length < 2) return true
+
+  for (let i = 1; i < rev.length; i++) {
+	candidate = candidate.concat(rev[i]);
+	if (str.indexOf(candidate) !== -1) return true
+  }
+
+  return false;
 }
+
 
 module.exports = permPalin;
