37bec7bd50d7d5298e27f9c0ecbb899b867caee2
timjj92 <timjj92@gmail.com>
2019-11-09 09:33:11 -0800

    perm-palin attempt 1

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -3,14 +3,26 @@
  *
  * Example:
  * 	- permPalin('abab') => true
- * 	- permPalin('cbaba') => true
+ * 	- permPalin('czaba') => true
  * 	- permPalin('cbac') => false
  * 	- permPalin('a') => true
  *
  */
 
 function permPalin(str) {
-	
+  if(!str) return false;
+  if(str.length === 1) return true;
+  for(let i = 0; i < str.length; i++){
+    if(str[i + 1]){
+      if(str[i] === str[i+1])
+        return true;
+    }
+    if(str[i + 2]){
+      if(str[i] === str[i+2])
+        return true;
+    }
+  }
+  return false; 
 }
 
 module.exports = permPalin;
