3b31b8a6b2dbbffb6c4b0609a42b2b3116cd27c8
Jason Yu <jyu.json@gmail.com>
2019-11-09 10:00:29 -0800

    attempt 1 for perm palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,27 @@
  */
 
 function permPalin(str) {
-	
+    if (typeof str !== "string" || str.length === 0) return false;
+    if (str.length === 1) return true;
+
+    if (str.length % 2 === 0) {
+        for (let i = 0; i < str.length; i++) {
+            for (let j = i; j < str.length; j++) {
+                if (str[j] === str[j+3] && str[j+1] === str[j+2]) return true;
+            }
+        }
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i; j < str.length; j++) {
+            if (str[j] === str[j+2]) return true;
+        }
+    }   
+
+    return false;
 }
+
+// console.log(permPalin('cbaba'))
+// console.log(permPalin('abab'))
 
 module.exports = permPalin;
