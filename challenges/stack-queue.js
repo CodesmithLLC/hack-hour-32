b713f65d84fc4ca93b9c406f8068d293d447aeaf
Scott McInnis <scottalyst@gmail.com>
2019-11-12 09:29:50 -0800

    Hack Hour: Stack Queue, not sure what the question is

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,8 +4,28 @@
 
 
 function Stack() {
-
+	this.stack = {};
+	this.length = 0;
 }
+
+Stack.prototype = {
+	push: function(element) {
+		this.stack[this.length] = element;
+		this.length += 1;
+		return this.length;
+	}
+	pop: function() {
+		if (this.length > 0) {
+			let lastElement = this.length - 1;
+			let returnValue = this.stack[lastElement];
+			delete this.stack[lastElement];
+			this.length -= 1;
+			return returnValue;
+		} else {
+			return undefined;
+		}
+	}
+}  
 
 
 /**
@@ -14,7 +34,26 @@ function Stack() {
 
 
 function Queue() {
+	this.queue = {};
+	this.length = 0;
+}
 
+Queue.prototype = {
+	push: function(element) {
+		this.queue[this.length] = element;
+		this.length += 1;
+		return this.length;
+	}
+	shift: function() {
+		if (this.length > 0) {
+			let returnValue = this.queue[0];
+			delete this.queue[0];
+			this.length -= 1;
+			return returnValue;
+		} else {
+			return undefined;
+		}
+	}
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
