bf6cd89065c46000ad81f81bcdb95f74ac44146b
Justin Lo <justinkevinlo@gmail.com>
2019-11-12 09:08:32 -0800

    rotat grid completed

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -17,7 +17,24 @@
  */
 
 function rotateGrid(grid, n) {
+  // need to clear out ( reassign temp arr after every n elements);
+  let tempArr = [];
+  const holdArr = [];
+  //get the first element of every array. Starting from the last array. push these three into an array. push that array into an array.
+  //get the second element of every array. push these three into an array. push that array into an array.
+  //get the third element of every array. push these three into an array. push that array into an array.
 
+  for (let j = 0; j < n; j += 1) {
+    for (let i = n; i > 0; i -= 1) {
+      tempArr.push(grid[i - 1][j]);
+    }
+    holdArr.push(tempArr);
+    tempArr = [];
+  }
+  console.log(tempArr);
+  console.log(holdArr);
+  return holdArr;
 }
 
+console.log(rotateGrid([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3));
 module.exports = rotateGrid;
