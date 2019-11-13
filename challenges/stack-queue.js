9d54423bc3cc3dbff1ea61f644d75bdf0e5c8f9e
aburkett1 <austin_osten@hotmail.com>
2019-11-13 09:30:40 -0800

    Fixed queue length

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -29,7 +29,7 @@ Stack.prototype.pop = function() {
 function Queue() {
     this.stack1 = new Stack;
     this.stack2 = new Stack;
-    this.length = this.stack1.index + this.stack2.index;
+    this.length = 0;
 }
 
 Queue.prototype.enqueue = function(value) {
@@ -39,6 +39,7 @@ Queue.prototype.enqueue = function(value) {
             this.stack2.push(this.stack1.pop());
         }
     }
+    this.length = this.stack1.index + this.stack2.index;
     return this.length;
 }
 
@@ -53,13 +54,18 @@ Queue.prototype.dequeue = function() {
 }
 
 const myQ = new Queue;
-myQ.enqueue(1);
-myQ.enqueue(2);
-myQ.enqueue(3);
-myQ.enqueue(4);
+console.log(myQ.enqueue(1));
+console.log(myQ.enqueue(2));
+console.log(myQ.enqueue(3));
+console.log(myQ.enqueue(4))
+console.log(myQ.enqueue(500))
 console.log(myQ);
 
-myQ.dequeue();
+console.log(myQ.dequeue())
+console.log(myQ.dequeue())
+console.log(myQ.dequeue())
+console.log(myQ.dequeue())
+console.log(myQ.dequeue())
 console.log(myQ)
 
 module.exports = {Stack: Stack, Queue: Queue};
