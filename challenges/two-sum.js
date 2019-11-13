06a334e34ed81e0e58f22027a3f1f89418571492
aburkett1 <austin_osten@hotmail.com>
2019-11-13 09:46:33 -0800

    completed two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,16 @@
  */
 
 function twoSum(arr, n) {
-
+  const matches = new Set ([]);
+  for (let i = 0; i < arr.length; i++) {
+    if (matches.has(arr[i])) return true;
+    matches.add(n-arr[i]);
+  }
+  return false;
 }
+
+console.log(twoSum([1, 2, 4, 6], 5))
+console.log(twoSum([1, 2, 4, 6], 9))
+console.log(twoSum([1, -2, 4, 6], 2))
 
 module.exports = twoSum;
