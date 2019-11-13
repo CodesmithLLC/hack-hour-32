72ba37a2f5499856d360563b9a64e5bffba5f663
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2019-11-13 09:30:56 -0800

    faster

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,21 +3,34 @@
  */
 
 function twoSum(arr, n) {
+ 
+    let obj={}
 
+    //put arr in map
     //iterate arry/2 see if the difference is also there
     if(arr===undefined || arr.length===0){ return false}
+    if(n===undefined){ return false}
+    for(let i=0;i<arr.length;i++){
 
-    for(let i=0;i<arr.length/2+1;i++){
+        //alternative, put in map, if the difference value is there, then we have found it
         let el=arr[i]
-        if(arr.indexOf(n-el)>=0){ return true}
+        // if(arr.indexOf(n-el)>=0){ return true}
+        obj[el]=true;
+        if(obj[n-el]){
+            return true;
+        }
     }
+
+    
 
     return false
 }
 
 
-// let arr=[1,2,3,4,5,6,7]
-// console.log(twoSum(arr,8))
-// console.log(twoSum(arr,9))
-// console.log(twoSum(arr,90))
+let arr=[1,2,3,4,5,6,7]
+console.log(twoSum(arr,8))
+console.log(twoSum(arr,9))
+console.log(twoSum(arr,90))
+
+console
 module.exports = twoSum;
