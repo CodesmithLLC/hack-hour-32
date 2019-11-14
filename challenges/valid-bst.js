d6e6dd365bc37cd293ba49bc12dbd837f2f86177
Jason Yu <jyu.json@gmail.com>
2019-11-14 09:18:03 -0800

    1st attempt valid-bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -13,7 +13,13 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
+    // if a right or left node exist and the current node's value is greater than right node's value or less than left node's value, return false
+    if (this.right && this.value > this.right.value) return false;
+    if (this.left && this.value < this.left.value) return false;
 
+    // if this.right or this.left exist, continue traversing and call BST on existing node, otherwise we've reached the end and it's valid
+    return (this.right || this.left) ? validBST(this.right) || validBST(this.left) : true
 }
+
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
