4f8cc4191cf70ef8da2db1c8a12185b8c72225a9
Jason Yu <jyu.json@gmail.com>
2019-11-15 09:55:56 -0800

    refactored

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -11,24 +11,10 @@ function gcd(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') return 1;
     if (a === b) return a;
 
-    // brute force; find the largest int
-    if (a > b) {
-        // iteratively decrement the int (but start from 2nd number)
-        for (let i = b; i >= 0; i--) {
-            // test whether int/# has remainder of 0;
-            if (a % i === 0) {
-                // if so, divide the 2nd int by that # and check if 2nd int/# has remainder of 0 too;
-                if (Number.isInteger(b % i)) return i;
-            }
-        }
-    }
+    let greatestCD = 0;
 
-    if (b > a) {
-        for (let i = a; i >= 0; i--) {
-            if (a % i === 0) {
-                if (Number.isInteger(a % i)) return i;
-            }
-        }
+    for (let i = 0; i < Math.min(a,b); i++) {
+        if (a % i === 0 && b % i === 0) return i;
     }
 
     return 1;
