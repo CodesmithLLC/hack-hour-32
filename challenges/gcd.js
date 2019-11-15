41df9e91d561a4a9da5d97c22300a507f68ae9fc
Nobuhide Ajito <najito@Emis-MacBook.local>
2019-11-15 10:22:47 -0800

    gcd.js done

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,17 @@
  */
 
 function gcd(a, b) {
-
+    let temp = 0;
+    for (let i = 0; i <= a / 2; i ++) {
+        if (a % i === 0) {
+            if (b % i === 0) {
+                temp = i
+            }
+        }
+        
+    }
+    return temp;
 }
+
 
 module.exports = gcd;
