03a3344a9bc65d9fc7cea0a53eebc982ecabfd73
timjj92 <timjj92@gmail.com>
2019-11-15 09:25:56 -0800

    gcd.js attempt 1

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,13 @@
  */
 
 function gcd(a, b) {
-
+  let greatest = 0;
+  for(let i = 0; i < Math.min(a,b); i++){
+    if(a % i === 0 && b % i === 0)
+      greatest = i;
+  }
+  return greatest;
 }
 
+console.log(gcd(10, 9));
 module.exports = gcd;
